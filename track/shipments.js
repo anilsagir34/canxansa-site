1d240ee97dfcac03fb669dda1f12420007b7392a
Anıl <anil@Sla-MacBook-Air.local>
2026-05-14 14:46:01 +0300

    track: CX260001 durum guncellendi, depoda bekliyor

--- track/shipments.js
+++ track/shipments.js
@@ -10,7 +10,7 @@ var SHIPMENTS={
     {status:"Booking Confirmed",date:"2026-05-12",location:"Istanbul",done:true},
     {status:"Cargo Collected",date:"2026-05-13",location:"Istanbul",done:true},
     {status:"At Warehouse",date:"2026-05-14",location:"Büyükçekmece Depo, Istanbul",done:true},
-    {status:"Customs Clearance",date:"2026-05-14",location:"Istanbul",done:true},
+    {status:"Customs Clearance",date:null,location:"Istanbul",done:false},
     {status:"Departed",date:null,location:"Istanbul",done:false},
     {status:"In Transit",date:null,location:"",done:false},
     {status:"At Destination",date:null,location:"Nove Mesto Nad Vahom",done:false},
